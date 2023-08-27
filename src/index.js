af74573ca8e6bd1f493342e711d240e62fd3a1b7
kienpc9x <kienpc9x@gmail.com>
2023-08-27 23:31:08 +0700

    remove redux

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore } from "redux";
-import { Provider } from "react-redux";
-import appReducer from './reducers';
 
 import './styles.scss';
 
@@ -18,15 +15,11 @@ import '@fontsource/roboto/700.css';
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
-const store = createStore(appReducer);
-
 root.render(
-    <Provider store={store}>
         <BrowserRouter>
             <Layout>
                 <App />
             </Layout>
         </BrowserRouter>
-    </Provider>
 );
 reportWebVitals();
