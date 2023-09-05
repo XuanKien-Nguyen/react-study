ca81ef8edafd792505ef742ea33e6cd36de849aa
XuanKien-Nguyen <73980002+XuanKien-Nguyen@users.noreply.github.com>
2023-09-05 13:25:08 +0700

    axios interceptor

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
 
 import './styles.scss';
 
@@ -11,6 +12,20 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+
+axios.defaults.baseURL = 'https://localhost:8080';
+
+axios.interceptors.request.use((config) => {
+    return config;
+}, (error) => {
+    return Promise.reject(error)
+});
+
+axios.interceptors.response.use((respone) => {
+    return response;
+}, (error) => {
+    return Promise.reject(error);
+})
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
